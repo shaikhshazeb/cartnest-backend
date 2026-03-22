8dede2b4962e39f1ce5d1b640460397a00f6440f
Shaikh Shazeb <shazebs077@gmail.com>
2026-03-22 23:33:59 +0530

    Admin Can update the status

--- CartNest/src/main/java/com/sss/cartnest/entities/OrderStatus.java
+++ CartNest/src/main/java/com/sss/cartnest/entities/OrderStatus.java
@@ -1,8 +1,11 @@
 package com.sss.cartnest.entities;
 
 public enum OrderStatus {
-	
-	PENDING,
-	SUCCESS,
-	FAILED
+    PENDING,
+    SUCCESS,
+    FAILED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
 }
